f9992b254a4d63737b35801b60298597d6f9ca78
Md Rabbi <mdrabbi638383@gmail.com>
2025-10-23 00:30:07 +0600

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,35 @@
+const mainContainer = document.querySelector(".main-container");
+const openMenuBtn = document.querySelector(".open_menu-icon");
+const closeMenuBtn = document.querySelector(".close_menu-icon");
+const sideBar = document.querySelector(".navbar_links");
+
+openMenuBtn.addEventListener("click",() => {
+    mainContainer.classList.add("overlay");
+    sideBar.classList.add("open-sidebar");
+    document.body.classList.add("no-scroll");
+})
+
+closeMenuBtn.addEventListener("click",() => {
+    mainContainer.classList.remove("overlay");
+    sideBar.classList.remove("open-sidebar");
+    document.body.classList.remove("no-scroll");
+
+})
+
+const mainMenus = document.querySelectorAll(".mainMenu");
+
+mainMenus.forEach(mainMenu => {
+    mainMenu.addEventListener("click", () => {
+        mainMenu.classList.toggle("active");
+    })
+} )
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 850){ 
+        mainContainer.classList.remove("overlay");
+    } else{
+        if(sideBar.classList.contains("open-sidebar")){
+            mainContainer.classList.add("overlay");
+        }
+    }
+})
